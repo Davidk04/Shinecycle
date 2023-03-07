7a8f9af1b35f54f12a11c19c96bffebde6f21846
Krešic David <David.Kresic@swisscom.com>
2023-03-07 09:41:10 +0100

    added starting page and timer

---
+++ app/src/main/java/com/example/shinesensor/StartActivity.java
@@ -0,0 +1,28 @@
+package com.example.shinesensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class StartActivity extends AppCompatActivity {
+    private Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        //Code from stack overflow: https://stackoverflow.com/questions/24745546/android-change-activity-after-few-seconds
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartActivity.this, InputActivity.class);
+                startActivity(intent);
+            }
+        }, 5000);
+    }
+}
